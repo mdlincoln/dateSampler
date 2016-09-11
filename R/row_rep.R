573581822f93d0bafad00c7e516cf3d9cb7f3b82
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2016-09-11 10:44:58 -0700

    row_rep adds replicate ID column
    supplied as the parameter .id

--- R/row_rep.R
+++ R/row_rep.R
@@ -1,6 +1,9 @@
 #' Repeat rows
 #'
 #' @export
-row_rep <- function(df, n) {
-  df[rep(1:nrow(df), each = n),]
+row_rep <- function(df, n, .id = "replicate") {
+  m <- nrow(df)
+  ndf <- df[rep(seq_len(m), each = n),]
+  ndf[[.id]] <- rep(seq_leng(n), each = m)
+  return(ndf)
 }
