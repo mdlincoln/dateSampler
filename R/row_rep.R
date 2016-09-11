5cc954aa600bb7720c2457f87f24a9609041a8fb
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2016-09-11 10:45:07 -0700

    document row_rep completely

--- R/row_rep.R
+++ R/row_rep.R
@@ -1,6 +1,19 @@
 #' Repeat rows
 #'
+#' Replicate the rows of a data frame n times.
+#'
+#' @param df A data frame
+#' @param n Number of replicates to create
+#' @param .id Name of the oclumn in which to store the integer replicate ID
+#'
+#' @return A data frame
+#'
 #' @export
+#'
+#' @examples
+#' rep_iris <- row_rep(iris, 5)
+#' names(rep_iris)
+#' nrow(rep_iris)
 row_rep <- function(df, n, .id = "replicate") {
   m <- nrow(df)
   ndf <- df[rep(seq_len(m), each = n),]
