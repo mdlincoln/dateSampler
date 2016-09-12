ffd916f82bcaba87b2953c788eeef4add284370e
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2016-09-12 13:09:02 -0700

    make sample_date_df take ...

--- R/sample_date_df.R
+++ R/sample_date_df.R
@@ -3,26 +3,29 @@
 #' Requires dplyr
 #'
 #' @param df The data frame to augment
-#' @param y The column of \code{df} containing the year
-#' @param m The column of \code{df} containing the month
-#' @param d The column of \code{df} containing the day
-#' @param n The number of replications
-#' @param col_name Name of the new column with sampled dates to add to \code{df}
+#' @param n Integer. The number of replications
+#' @param .name Name of the new column with sampled dates to add to \code{df}
+#' @param ... Arguments to pass to \code{\link{sample_date}}
+#' @param .id Name of the new column with iteration ids to add to \code{df}
+#' @param quiet Boolean.
 #'
-#' @return A data frame
+#' @return The replicated data frame \code{df} with two new columns.
 #'
 #' @import dplyr
 #'
 #' @export
-sample_date_df <- function(df, y, m, d, n, col_name = "sampled_date") {
+sample_date_df <- function(df, n, .name = "sampled_date", .id = "replicate", quiet = TRUE, ...) {
+  assertthat::is.count(n)
+
   # Generate a dataframe with new dates
   new_dates <- df %>%
     rowwise() %>%
-    do(data.frame(new_date = sample_date(y = .[[y]], m = .[[m]], d = .[[d]], n = n)))
+    do(data.frame(d = sample_date(..., n = n, quiet = quiet)))
+  names(new_dates) <- .name
 
   # Replicate the rows of df
   exp_df <- df %>%
-    row_rep(n = n)
+    row_rep(n = n, .id = .id)
 
   # Join the new dates onto the original dataframe
   bind_cols(exp_df, new_dates)
